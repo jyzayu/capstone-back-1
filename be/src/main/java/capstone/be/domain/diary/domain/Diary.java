e9d6e666f6e6dfb42f88873a190882f4f2a9b19c
Unknown <moosou30@naver.com>
2023-05-25 00:00:53 +0900

    feat: hashtag Entity 구현
    diary에서 string으로 구현한 것 entity 로  구현, 태그 목록 조회 등 추가 기능을 구현하도록 확장할 때 구현하기 좋을 것 같다. @Getter 사용하여 간결하게 수정

--- be/src/main/java/capstone/be/domain/diary/domain/Diary.java
+++ be/src/main/java/capstone/be/domain/diary/domain/Diary.java
@@ -1,13 +1,19 @@
 package capstone.be.domain.diary.domain;
 
+import capstone.be.domain.hashtag.domain.Hashtag;
 import capstone.be.global.entity.AuditingFields;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-
+@Getter
+@ToString(callSuper = true)
 @Entity
 public class Diary extends AuditingFields {
 
@@ -20,27 +26,29 @@ public class Diary extends AuditingFields {
     @Setter
     private String weather;
 
-    @Setter
-    private String hashtag;
+    @ToString.Exclude
+    @JoinTable(
+            name = "diary_hashtag",
+            joinColumns = @JoinColumn(name = "diaryId"),
+            inverseJoinColumns = @JoinColumn(name = "hashtagId")
+    )
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Hashtag> hashtags = new LinkedHashSet<>();
     @Setter
     private String mood;
-
-
-    //Todo: Block 구현 Entity로 구현하면 될듯 
-
-    private Diary(String title, String weather, String hashtag, String mood) {
-        this.title = title;
-        this.weather = weather;
-        this.hashtag = hashtag;
-        this.mood = mood;
-    }
 
     public Diary() {
 
     }
 
-    public static Diary of(String title, String weather, String hashtag, String mood){
-        return new Diary(title, weather, hashtag, mood);
+    private Diary(String title, String weather, String mood) {
+        this.title = title;
+        this.weather = weather;
+        this.mood = mood;
+    }
+
+    public static Diary of(String title, String weather, String mood){
+        return new Diary(title, weather, mood);
     }
     //hashtag의 경우 필드에 string으로 저장시 태그목록 조회하면서 hashtag 값 전체를 조회하는데 diary에 들어있으면 diary를 전부 조회해야하고, 그 안에 잇는 값을 저장하는 로직을 만들어야함
     // hashtag entity를 만들어 diary와 연관관계 설정을 해준다.
@@ -49,23 +57,16 @@ public class Diary extends AuditingFields {
     // setter는 필요한 필드에만 붙여서 사용합니다.
 
 
-    public Long getId() {
-        return id;
+    public void addHashtag(Hashtag hashtag) {
+        this.getHashtags().add(hashtag);
     }
 
-    public String getTitle() {
-        return title;
+    public void addHashtags(Collection<Hashtag> hashtags) {
+        this.getHashtags().addAll(hashtags);
     }
 
-    public String getWeather() {
-        return weather;
+    public void clearHashtags() {
+        this.getHashtags().clear();
     }
 
-    public String getHashtag() {
-        return hashtag;
-    }
-
-    public String getMood() {
-        return mood;
-    }
 }
