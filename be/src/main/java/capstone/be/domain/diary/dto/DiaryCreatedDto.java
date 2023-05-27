5eda0cf334672f473166d8ab1a3f85d3cb9284ca
Unknown <moosou30@naver.com>
2023-05-27 22:36:31 +0900

    feat: hashtag 응답 수정
    hashtag 객체를 보내는 것이 아닌 name만 보내도록 수정

--- be/src/main/java/capstone/be/domain/diary/dto/DiaryCreatedDto.java
+++ be/src/main/java/capstone/be/domain/diary/dto/DiaryCreatedDto.java
@@ -3,6 +3,7 @@ package capstone.be.domain.diary.dto;
 import capstone.be.domain.diary.domain.BProperties;
 import capstone.be.domain.diary.domain.Diary;
 import capstone.be.domain.hashtag.dto.HashtagDto;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 import java.time.LocalDateTime;
 import java.util.Set;
@@ -12,7 +13,7 @@ import java.util.List;
 public record DiaryCreatedDto(
         String title,
         String weather,
-        Set<HashtagDto> hashtag,
+        Set<String> hashtag,
         String mood,
 
         String font,
@@ -24,9 +25,10 @@ public record DiaryCreatedDto(
         return Diary.of(title, weather, mood, font, blocks);
     }
 
+    @JsonCreator
     public static DiaryCreatedDto from(Diary diary){
         return new DiaryCreatedDto(diary.getTitle(), diary.getWeather(),
-                diary.getHashtags().stream().map(HashtagDto::from).collect(Collectors.toUnmodifiableSet()),
+                diary.getHashtags().stream().map(HashtagDto::from).map(HashtagDto::getHashtagName).collect(Collectors.toUnmodifiableSet()),
                 diary.getMood(),
                 diary.getFont(),
                 diary.getBlocks());
