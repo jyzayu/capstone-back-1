9f1416e7b09d0c704792a0b4d6e2a5cd6a3856c5
Unknown <moosou30@naver.com>
2023-05-29 23:28:50 +0900

    feat: hashtagNames를 응답으로 수정
    hashtag객체가 응답으로 전달됐는데, names만 전달되도록 수정

--- be/src/main/java/capstone/be/domain/diary/dto/DiaryCreatedDto.java
+++ be/src/main/java/capstone/be/domain/diary/dto/DiaryCreatedDto.java
@@ -2,6 +2,7 @@ package capstone.be.domain.diary.dto;
 
 import capstone.be.domain.diary.domain.BProperties;
 import capstone.be.domain.diary.domain.Diary;
+import capstone.be.domain.hashtag.domain.Hashtag;
 import capstone.be.domain.hashtag.dto.HashtagDto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
@@ -21,7 +22,7 @@ import java.util.List;
 public record DiaryCreatedDto(
         String title,
         String weather,
-        Set<HashtagDto> hashtag,
+        Set<String> hashtag,
         String mood,
 
         String font,
@@ -39,7 +40,7 @@ public record DiaryCreatedDto(
     public static DiaryCreatedDto from(Diary diary){
 
         return new DiaryCreatedDto(diary.getTitle(), diary.getWeather(),
-                diary.getHashtags().stream().map(HashtagDto::from).collect(Collectors.toUnmodifiableSet()),
+                diary.getHashtags().stream().map(Hashtag::getHashtagName).collect(Collectors.toUnmodifiableSet()),
                 diary.getMood(),
                 diary.getFont(),
                 diary.getBlocks(),
