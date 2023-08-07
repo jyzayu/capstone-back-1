f67a3b48882bb99228afca4f707e8855ebbdfe32
배두리 <78518155+2ralumin@users.noreply.github.com>
2023-08-07 20:39:44 +0900

    api 지우기

--- be/src/main/java/capstone/be/domain/user/controller/SignController.java
+++ be/src/main/java/capstone/be/domain/user/controller/SignController.java
@@ -34,7 +34,6 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
 public class SignController {
 
     private final SignService signService;
