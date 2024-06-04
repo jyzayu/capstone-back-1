a0a81eaba9bc03220efbf88b44f11530e895cdf2
jyzayu <moosou30@naver.com>
2024-06-04 20:10:20 +0900

    feat: redis 설정 @Cacheable 사용 위해 cacheManager 빈 생성, List 캐싱을 위해 StringSerializer로 변경, 게시글과 인기글 제목,id 캐싱을 위한 template 생성

--- be/src/main/java/capstone/be/global/config/RedisRepositoryConfig.java
+++ be/src/main/java/capstone/be/global/config/RedisRepositoryConfig.java
@@ -1,19 +1,24 @@
 package capstone.be.global.config;
 
 import capstone.be.domain.diary.domain.SearchLog;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisRepositoryConfig {
@@ -25,6 +30,18 @@ public class RedisRepositoryConfig {
 
 
 
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofHours(6L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory())
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
+
+
     // lettuce
     // RedisConnectionFactory 인터페이스를 통해 LettuceConnectionFactory를 생성하여 반환한다.
     // RedisProperties로 yaml에 저장한 host, post를 가지고 와서 연결한다.
@@ -34,15 +51,35 @@ public class RedisRepositoryConfig {
     }
 
     // setKeySerializer, setValueSerializer 설정으로 redis-cli를 통해 직접 데이터를 보는게 가능하다.
+
+
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
+    public RedisTemplate<Long, Object> postTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Long, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // Configure the RedisTemplate
+        template.setKeySerializer(new GenericToStringSerializer<>(Long.class));
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new GenericToStringSerializer<>(Long.class));
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+
+        return template;
     }
 
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // Configure the RedisTemplate
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+
+        return template;
+    }
     @Bean
     public RedisTemplate<String, SearchLog> searchLogRedis() {
         RedisTemplate<String, SearchLog> redisTemplate = new RedisTemplate<>();
