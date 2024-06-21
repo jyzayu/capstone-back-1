a5a00a69a641aa6b28ea0df8ba39086a4f1279da
jyzayu <moosou30@naver.com>
2024-06-21 11:18:10 +0900

    Removed cached files

--- be/src/main/java/capstone/be/domain/hashtag/repository/HashtagRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package capstone.be.domain.hashtag.repository;
-
-
-import java.util.List;
-
-public interface HashtagRepositoryCustom {
-    List<String> findAllHashtagNames();
-}
--- be/src/main/java/capstone/be/domain/user/dto/LoginResponseDto.java
+++
@@ -1,8 +0,0 @@
-package capstone.be.domain.user.dto;
-
-public record LoginResponseDto(
-        String accessToken,
-        String refreshToken,
-        Boolean newUser) {
-
-}
